14b80f435354385f0eb8bc2a1e1f013b34ad8f75
MINHTAM\MinhTam <phanminhtam247@gmail.com>
2017-09-14 14:53:39 +0700

    finish Home work

--- HW_1/lib/ruby_intro.rb
+++ HW_1/lib/ruby_intro.rb
@@ -23,27 +23,42 @@ def max_2_sum arr
 end
 
 def sum_to_n? arr, n
-  result = false
-  (0..arr.length - 2).one?{|i| n == arr[i] + arr[i + 1]}
-  return result
+  (1..arr.length - 2).one?{|i| n == arr[i] + arr[i + 1]}
 end
 
 # Part 2
 
 def hello(name)
   # YOUR CODE HERE
+  return "Hello, " + name
 end
 
 def starts_with_consonant? s
   # YOUR CODE HERE
+  s = s.downcase
+  return false if s == ""
+  return !s.start_with?("a", "e", "i", "o", "u") if s[0].ord > 96
+  return false
 end
 
 def binary_multiple_of_4? s
   # YOUR CODE HERE
+  return false if s == ""
+  s.each_char{|c| return false if c != '0' and c != '1'}
+  return s.to_i(2) % 4 == 0
 end
 
 # Part 3
 
 class BookInStock
 # YOUR CODE HERE
+    attr_accessor :isbn
+    attr_accessor :price
+    def initialize(isbn, price)
+      raise ArgumentError,'Argument Error' if isbn == '' or price <= 0
+      @isbn, @price = isbn, price
+    end 
+    def price_as_string
+      return "$#{'%.2f' % self.price}"
+    end
 end

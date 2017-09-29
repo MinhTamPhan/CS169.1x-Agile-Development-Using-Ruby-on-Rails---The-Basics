e2bb2b1424507b3e9bcd31a89c64bd191c5a7d02
MINHTAM\MinhTam <phanminhtam247@gmail.com>
2017-09-29 22:53:25 +0700

    remove Slide

--- HW_4/app/models/movie.rb
+++
@@ -1,8 +0,0 @@
-class Movie < ActiveRecord::Base
-    def self.get_rating()
-        Movie.select(:rating).distinct
-    end
-    def self.all_ratings
-        %w(G PG PG-13 NC-17 R)    
-    end
-end
